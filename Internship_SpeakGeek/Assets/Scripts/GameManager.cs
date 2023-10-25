cd8d7aa16d93d7ef58a6319b2512a98727c7e24c
MalakaiBraam <140538653+MalakaiBraam@users.noreply.github.com>
2023-10-25 14:25:56 +0200

    Timer

--- Internship_SpeakGeek/Assets/Scripts/GameManager.cs
+++ Internship_SpeakGeek/Assets/Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
     public Vector3 MostRecentCheck;
-
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
 
     private void Awake()
     {
@@ -19,6 +21,14 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
 
--- Internship_SpeakGeek/Assets/Scripts/UIManager.cs
+++ Internship_SpeakGeek/Assets/Scripts/UIManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    private string input;
+    public static string input;
 
     public void LoadGame()
     {
