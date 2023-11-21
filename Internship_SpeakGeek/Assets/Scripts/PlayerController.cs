f4bad74d0bd9cf3accb058462a6befe8074658ec
MalakaiBraam <140538653+MalakaiBraam@users.noreply.github.com>
2023-11-22 00:16:03 +0200

    Ball and Camera Movement

--- Internship_SpeakGeek/Assets/Scripts/PlayerController.cs
+++ Internship_SpeakGeek/Assets/Scripts/PlayerController.cs
@@ -1,83 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    private GameManager gM;
     public float speed;
-    public Rigidbody rb;
-   // public GameObject health1;
-   // public GameObject health2;
-   // public GameObject health3;
-    public int health;
-    public bool canJump = false;
-    public float jumpForce = 5.0f;
-    private float lastJumpTime;
-    public float jumpCooldown = 2.0f;
+
+    private Rigidbody rigidBody;
 
     private void Start()
     {
-        gM = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
-        transform.position = gM.MostRecentCheck;
-        health = 3;
-    }
-    private void Update()
-    {
-        
-        if(Input.GetAxis("Horizontal") > 0)
-        {
-            rb.AddForce(Vector3.right * speed);
-        }
-        else if (Input.GetAxis("Horizontal") < 0)
-        {
-            rb.AddForce(Vector3.left * speed);
-        }
-        else if (Input.GetAxis("Vertical") > 0)
-        {
-            rb.AddForce(Vector3.forward * speed);
-        }
-        else if (Input.GetAxis("Vertical") < 0)
-        {
-            rb.AddForce(Vector3.back * speed);
-        }
-
-      /*  if (health == 2)
-        {
-            health1.SetActive(false);
-        }
-        else if (health == 1)
-        {
-            health2.SetActive(false);
-        }
-        else if (health == 0)
-        {
-            health3.SetActive(false);
-            SceneManager.LoadScene(2);
-            
-        }*/
-
-        if (canJump && Input.GetKeyDown(KeyCode.Space) && (Time.time - lastJumpTime) >= jumpCooldown)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            lastJumpTime = Time.time; 
-        }
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void FixedUpdate()
     {
-       /* if (collision.gameObject.CompareTag("Boundary"))
-        {
-            transform.position = gM.MostRecentCheck;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-            if (health >= 1)
-            {
-                health = health - 1;
-            }
-            
-        }*/
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-
+        rigidBody.AddForce(movement * speed);
     }
 }
